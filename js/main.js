37bed8e505a6421cc65b1c8f4f460041ef2380ae
Shohruzbekprog <shokhruz.akhe@gmail.com>
2023-07-05 14:13:55 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 50,
+  nav: true,
+  dots: true,
+  // center: true,
+  slideBy: 2,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    500: {
+      items: 1,
+    },
+    900: {
+      items: 3,
+    },
+    1200: {
+      items: 2,
+    },
+  },
+});
